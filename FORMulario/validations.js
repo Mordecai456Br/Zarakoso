89fc0532c8246aba154d77125e3933d896616760
Gabriel Freitas <62771225+Mordecai456Br@users.noreply.github.com>
2025-08-31 20:16:06 -0300

    Formataçao Data e CPF

--- FORMulario/validations.js
+++ FORMulario/validations.js
@@ -1,18 +1,42 @@
 
-const nameInput = document.querySelector('#nameInput')
+const nameInput = document.querySelector('#nameInput');
 const dateInput = document.querySelector('#dateInput');
 
-const emailInput = document.querySelector('#emailInput')
-const passwordInput = document.querySelector('#passwordInput')
+const emailInput = document.querySelector('#emailInput');
+const cpfInput = document.querySelector('#cpfInput');
 
 
-dateInput.addEventListener("input", () => {
 
-    let cleaned = dateInput.value.replace(/\D/g,'');
+dateInput.addEventListener("input", function() {
+
+    let cleaned = this.value.replace(/\D/g,'');
 
     cleaned = cleaned.replace(/^(\d{2})(\d)/, '$1/$2')
     .replace (/^(\d{2}\/\d{2})(\d)/, '$1/$2')
     .slice(0,10);
-    dateInput.value = cleaned;
+    this.value = cleaned;
 });
+
+cpfInput.addEventListener("input", function() {
+
+    let cpf = this.value.replace(/\D/g,'');
+
+    if (cpf.length > 9){
+        cpf = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})/, '$1.$2.$3-$4');
+    } else if (cpf.length > 6) {
+        cpf = cpf.replace(/(\d{3})(\d{3})(\d{1,3})/, '$1.$2.$3');
+    } else if (cpf.length > 3) {
+        cpf = cpf.replace(/(\d{3})(\d{1,3})/, '$1.$2');
+    }
+    this.value = cpf.slice(0,14);
+})
+
+
+/* tem q validar os 2 ultimos digitos
+ digito 1 -> pega os 9 primeiros numeros e multiplica o primeiro, por 10,
+ o segundo por 9.. até chegar em *2
+*/
+function validarCPF() {
+
+}
 
