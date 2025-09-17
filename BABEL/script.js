45acd1af7fe3eb2b8b223e03c7e0a97887dfd07d
Gabriel Freitas <62771225+Mordecai456Br@users.noreply.github.com>
2025-09-17 10:24:06 -0300

    Babel

---
+++ BABEL/script.js
@@ -0,0 +1,11 @@
+ReactDOM.render(
+    "Aloou, eu gosto de dinocats", document.getElementById('app')
+)
+ReactDOM.render(
+    "Aloou, eu gosto de dinocats MUITO", document.getElementById('app2')
+)
+ReactDOM.render(
+    <ul>
+        <li><a href="https://supabase.com/dashboard/project/eoomcvolfyxbtkmzffij/database/tables">supabase</a></li>
+    </ul>, document.getElementById('app3')
+)
